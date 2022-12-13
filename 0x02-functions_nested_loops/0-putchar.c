367c1db80020f756d5cc79b331884ffc6e56c292
kakanyero <kakanyero142@gmail.com>
2022-12-13 15:32:44 +0300

    donot copy

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main-puts -putcahr to the screen
+ *
+ * Return: always retun 0
+ */
+int main(void)
+{
+	printf("_putchar");
+	return (0);
+
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+thi sis a readme for the third project>README.md
+echo thi sis a readme for the third project
