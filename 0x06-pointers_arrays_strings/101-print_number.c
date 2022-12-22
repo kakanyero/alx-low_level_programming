325cfc519aebc28f2dc3b70370f2731f858e4547
KAKANYERO INNOCENT <kakanyero142@gmail.com>
2022-12-22 01:22:26 -0800

    hul

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,5 +1,4 @@
-nclude "main.h"
-
+#include "main.h"
 /**
  * print_number - print numbers chars
  *
@@ -8,32 +7,20 @@ nclude "main.h"
  * Return: 0
  *
  */
-
 void print_number(int n)
-
 {
-
 	unsigned int n1;
 
 	n1 = n;
 
 	if (n < 0)
-
 	{
-
 		_putchar('-');
-
 		n1 = -n;
-
 	}
-
 	if (n1 / 10 != 0)
-
 	{
-
 		print_number(n1 / 10);
 	}
-
-		_putchar((n1 % 10) + '0');
-
+	_putchar((n1 % 10) + '0');
 }
