67f12917274eb21b6cd5303f5a6036e590c79529
kakanyero <kakanyero142@gmail.com>
2023-01-04 17:58:21 +0300

    kaka is badder

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -15,9 +15,9 @@ int wildcmp(char *s1, char *s2)
 		}
 		return (*s2 == '\0');
 	}
-	if(*s2 == '*')
+	if (*s2 == '*')
 	{
-		return(wildcmp(s1+1, s2) || wildcmp(s1, s2+1));
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
 	}
 	else if (*s1 == *s2)
 	{
