787a4e250723d2fdfe331e1f0f052d635096f749
kakanyero <kakanyero142@gmail.com>
2023-01-30 19:19:02 +0300

    all again

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -9,6 +9,7 @@
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new;
+
 	new = malloc(sizeof(listint_t));
 	if (!new)
 		return (NULL);
--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -10,6 +10,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *new;
 	listint_t *temp = *head;
+
 	new = malloc(sizeof(listint_t));
 	if (!new)
 		return (NULL);
