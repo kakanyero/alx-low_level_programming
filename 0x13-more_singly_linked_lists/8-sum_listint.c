335d720f84f85e243bb9895461d252f7f97d7c1c
kakanyero <kakanyero142@gmail.com>
2023-01-30 19:48:59 +0300

    kaka

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -8,8 +8,8 @@
 int sum_listint(listint_t *head)
 {
 	int sum = 0;
-
 	listint_t *temp = head;
+
 	while (temp)
 	{
 		sum += temp->n;
--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,6 +14,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	listint_t *new;
 	listint_t *temp = *head;
+
 	new = malloc(sizeof(listint_t));
 	if (!new || !head)
 		return (NULL);
