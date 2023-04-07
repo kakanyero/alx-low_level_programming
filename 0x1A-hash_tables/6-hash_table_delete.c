10d7f32eab1545148b8eab069043bffb21af7492
kakanyero <kakanyero142@gmail.com>
2023-04-07 15:29:13 +0300

    to balls

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -8,6 +8,7 @@ void hash_table_delete(hash_table_t *ht)
 	hash_table_t *head = ht;
 	hash_node_t *node, *tmp;
 	unsigned long int i;
+
 	for (i = 0; i < ht->size; i++)
 	{
 		if (ht->array[i] != NULL)
